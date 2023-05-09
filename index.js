0110226014fb02619e7dcdc33fbd57730a3e0fc6
vinyborges <viny.bdj@gmail.com>
2023-05-08 22:24:07 -0700

    commit tree bc236189a806343f28b71fd6b6adeb46e54d7bbb

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'Mike';
+
+
+function upperCaseCustomerName(){
+
+    return customerName = customerName.toUpperCase();
+
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Carl';
+}
+
+
